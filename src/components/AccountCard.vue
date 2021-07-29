ce6611317653d788d70309ec5e35ef72be6354ce
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-07-29 12:50:14 -0400

    account card styling part 1

--- src/components/AccountCard.vue
+++ src/components/AccountCard.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <h1>Account Card</h1>
-  </div>
-  <div v-for="account in accounts" :key="account.id">
-    <h1>{{ account.name_first }} {{ account.name_last }}</h1>
-    <p>{{ account.email }}</p>
+  <div class="account-card-container">
+    <div class="account-card" v-for="account in accounts" :key="account.id">
+      <div class="account-card-left">
+        <img v-bind:src="account.picture" />
+        <h1>{{ account.name_first }}</h1>
+        <h1>{{ account.name_last }}</h1>
+      </div>
+      <div class="account-card-right">
+        <ul>
+          <li>Email: {{ account.email }}</li>
+          <li>Phone: {{ account.phone }}</li>
+          <li>Address: {{ account.address }}</li>
+          <li>Credit: {{ account.credit }}</li>
+          <li>Balance: {{ account.balance }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,3 +43,54 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.account-card {
+  border-radius: 25px;
+  box-shadow: 0px 12px 20px rgba(34, 35, 58, 0.5);
+  width: 450px;
+  height: 250px;
+  background-color: rgba(25, 37, 104, 0.9);
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin: 25px;
+  padding: 10px 20px 10px 20px;
+}
+
+.account-card-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.account-card img {
+  top: 25px;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+}
+
+.account-card h1 {
+  margin: 0;
+}
+
+.account-card-left {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.account-card-right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.account-card-right ul {
+  list-style-type: none;
+}
+</style>
