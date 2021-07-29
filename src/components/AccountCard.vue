4017a04ba775307d614ec502c73c93104a33d5a8
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-07-29 09:13:53 -0400

    AccountCard component and  Accounts view page created; need to work on them

---
+++ src/components/AccountCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Account Card</h1>
+  </div>
+</template>
---
+++ src/views/Accounts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="accounts">
+    <h1>This is an accounts page</h1>
+    <AccountCard />
+  </div>
+</template>
+
+<script>
+import AccountCard from "../components/AccountCard.vue";
+
+export default {
+  name: "Accounts",
+  components: {
+    AccountCard,
+  },
+};
+</script>
