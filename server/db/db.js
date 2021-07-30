0a29de55046f6ae521815a39c11ffc9f2ba3ce45
Seong Jin Kim <seongjinkimsjk@gmail.com>
2021-07-30 09:58:18 -0400

    db set ssl to true

--- server/db/db.js
+++ server/db/db.js
@@ -8,7 +8,9 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
-    dialectOptions: {},
+    dialectOptions: {
+      ssl: true,
+    },
     ssl: true,
   }
 );
